59ae7880c40e85a2e4caeee6d927ecc5e3258950
Logan <goatmanwaffles@gmail.com>
2025-11-23 02:50:49 -0500

    Initial Files

---
+++ bankersAlgorithm.cpp
@@ -0,0 +1,9 @@
+//Let N be the number of processes in the system and M be the number of resource types
+
+//Data Structures
+
+//Available: 1D array of size M holds number of abailable resources of each type
+
+//Max: 2D array of size N*M that hold the maximum demand of each process in a system
+
+//Allocation
